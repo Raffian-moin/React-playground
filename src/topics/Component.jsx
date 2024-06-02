96c84523089efdbdab813beb71a781798c4e7c92
Raffian-moin <raffianmoin69@gmail.com>
2024-06-02 22:02:04 +0600

    Component and how to define components and dynamic values inside JSX

---
+++ src/topics/Component.jsx
@@ -0,0 +1,11 @@
+// Component name must start with Upper case letter
+// Component has only one tag that is self closing - i.e. <MyComponent />
+// When we want to use JavaScript code inside JSX we need to use {} to escape from JSX
+// Here div style should be dynamic, first {} is for escaping form JSX
+// second {} is for Javascript object. Here object is the styling attributes
+
+export default function Component() {
+    return (
+        <div style={{ color: 'red', fontSize: '20px' }}>Component</div>
+    )
+}
